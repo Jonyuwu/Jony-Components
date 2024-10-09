9bdd7e8241f85b8f060d764d3544db38f4d8b233
Jonyuwu <jonathanjesusabrajanhau@gmail.com>
2024-10-09 15:07:06 -0500

    New colors palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,34 @@
 export default {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        //Pimary colors scale
+        primary_1: '#FAE0E4',
+        primary_2: '#F7CAD0',
+        primary_3: '#F9BEC7',
+        primary_4: '#FBB1BD',
+        primary_5: '#FF99AC',
+        primary_6: '#FF85A1',
+        primary_7: '#FF7096',
+        primary_8: '#FF5C8A',
+        primary_9: '#FF477E',
+        primary_10: '#FF0A54',
+
+        //Secondary colors scale
+        secondary_1: '#50B9EE',
+        secondary_2: '#3FB2EC',
+        secondary_3: '#2CAAEA',
+        secondary_4: '#17A2E8',
+        secondary_5: '#0099E6',
+        secondary_6: '#008FD7',
+        secondary_7: '#007AB8',
+        secondary_8: '#005F8F',
+        secondary_9: '#004466',
+        secondary_10: '#003E5D'
+      }
+    }
   },
+
   plugins: []
 }
